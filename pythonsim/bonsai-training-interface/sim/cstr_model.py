2deac98ecb6e485fca51c5a6cd670d59180f9777
Juan Vergara <40925447+juanvergaramunoz@users.noreply.github.com>
2022-12-08 16:07:06 -0800

    Reformat cstr_model
    - add comments along the code.
    - removed unused variables ΔT, ΔCa.
    - standardize into Cr, Tr, and Tc, for state vars.
    - make solver function a class method.

--- pythonsim/bonsai-training-interface/sim/cstr_model.py
+++ pythonsim/bonsai-training-interface/sim/cstr_model.py
@@ -6,29 +6,33 @@ from scipy.integrate import odeint
 
 π = math.pi
 
+
 @dataclass
-class CSTRModel:
+class CSTR_Solver:
 
-    T: float  # Kelvin
-    Tc: float  # Kelvin
-    Ca: float #kmol/m3
-    ΔTc: float
-    ΔT: float = 0  # Kelvin
-    ΔCa: float = 0 #kmol/m3
-    
+    # Problem states & actions.
+    Cr: float       # Concentration at the reactor's output (kmol/m3)
+    Tr: float       # Temperature at the reactor's output (Kelvin)
+    Tc: float       # Temperature of the coolant (Kelvin)
+    ΔTc: float      # Delta action to apply to the temperature of the coolant.
+
+
+    # Running parameters
+    n: int = 3      # Number of runs to perform to resolve the ODE: Ordinary Differential Equation.
     
 
-    #constants
-    F: float = 1 #Volumetric flow rate (m3/h)
-    V: float = 1 #Reactor volume (m3)
-    k0: float = 34930800 #Pre-exponential nonthermal factor (1/h)
-    E: float = 11843 #Activation energy per mole (kcal/kmol)
-    R: float = 1.98588 #1.985875 #Boltzmann's ideal gas constant (kcal/(kmol·K))
-    ΔH: float = -5960 #Heat of reaction per mole kcal/kmol
-    phoCp: float = 500 #Density multiplied by heat capacity (kcal/(m3·K))
-    UA: float = 150 #Overall heat transfer coefficient multiplied by tank area (kcal/(K·h))
-    Cafin: float = 10 #kmol/m3
-    Tf: float = 298.2 #K
+    # Problem constants.
+    F: float = 1 # Volumetric flow rate (m3/h)
+    V: float = 1 # Reactor volume (m3)
+    k0: float = 34930800    # Pre-exponential nonthermal factor (1/h)
+    E: float = 11843        # Activation energy per mole (kcal/kmol)
+    R: float = 1.98588  # Boltzmann's ideal gas constant (kcal/(kmol·K))
+    ΔH: float = -5960   # Heat of reaction per mole kcal/kmol
+    phoCp: float = 500  # Density multiplied by heat capacity (kcal/(m3·K))
+    UA: float = 150     # Overall heat transfer coefficient multiplied by tank area (kcal/(K·h))
+    Cafin: float = 10   # kmol/m3
+    Tf: float = 298.2   # K
+
 
     def __post_init__(self):
         """""
@@ -38,45 +42,60 @@ class CSTRModel:
 
 
     def run_sim(self):
+        """""
+        Method that queues the computation of the ODE (ordinary differential equation).
+        """""
 
-        def model(z, t, u):
-            x = z[0]
-            y = z[1]
-            dxdt = (self.F/self.V * (self.Cafin - x)) - (self.k0 * exp(-self.E/(self.R*y))*x)
-            dydt =  (self.F/self.V *(self.Tf-y)) - ((self.ΔH/self.phoCp)*(self.k0 * exp(-self.E/(self.R*y))*x)) - ((self.UA /(self.phoCp*self.V)) *(y - self.Tc + u))
+        # Setup current conditions.
+        # TODO: Make this depend on the stepping time.
+        # TODO: Question for Octavio: Why do we run the solver for 3 iterations? Do we know the right amount for the stepping time (0.5s VS 1s)?
+        if abs(self.ΔTc) > 10:
+            self.ΔTc = 10 * self.ΔTc/abs(self.ΔTc)
+            raise ValueError(f"Current provided value of {self.ΔTc} exceeds the max ramp value of 10.")
 
-            dzdt = [dxdt,dydt]
-            return dzdt
-
-        #initial cond
-        z0 = [self.Ca, self.T]
-        n = 3 #3 4
-        t = np.linspace(0,1,n)
-        #input
+        # Setup current conditions.
+        z0 = [self.Cr, self.Tr]
+        t = np.linspace(0, 1, self.n)
+        
+        # Setup the changing conditions to the coolant temp.
         #u = [0,self.ΔTc]
-        u = np.zeros(n)
+        u = np.zeros(self.n)
         u[0] = self.ΔTc #-1 0
 
-        #solution
+        # Setup output variables to store intermediate results.
         x = np.empty_like(t)
         y = np.empty_like(t)
-
+        # Initialize the first array values to whichever current conditions.
         x[0] = z0[0]
         y[0] = z0[1]
 
-        #solve ODE
-        for i in range(1,n):
+        # Run the solver for 'n' steps to solve the ODE (Ordinary Differential Equation).
+        for i in range(1,self.n):
             tspan = [t[i-1], t[i]]
-            z = odeint(model, z0, tspan, args=(u[i],))
+            z = odeint(self.model, z0, tspan, args=(u[i],))
             x[i] = z[1][0]
             y[i] = z[1][1]
             z0 = z[1]
-            
-        self.Ca = x[1]
-        self.T = y[1]
+        
+        # Extract the upadted values for reactor temp & conc, as well as coolant temp.
+        self.Cr = x[1]
+        self.Tr = y[1]
         self.Tc += self.ΔTc
-        
-        
+    
+
+    def model(self, z, t, u):
+        """""
+        Method that resolves the ODE (ordinary differential equation).
+        """""
+        x = z[0]
+        y = z[1]
+        dxdt = (self.F/self.V * (self.Cafin - x)) - (self.k0 * exp(-self.E/(self.R*y))*x)
+        dydt =  (self.F/self.V *(self.Tf-y)) - \
+                ((self.ΔH/self.phoCp)*(self.k0 * exp(-self.E/(self.R*y))*x)) - \
+                ((self.UA /(self.phoCp*self.V)) * (y - self.Tc + u))
+
+        dzdt = [dxdt,dydt]
+        return dzdt
             
             
 
