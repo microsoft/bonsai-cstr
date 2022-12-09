3356f62f4826bc8b3beb44c0200ab200eadd34ab
Juan Vergara <40925447+juanvergaramunoz@users.noreply.github.com>
2022-12-08 16:18:46 -0800

    Reformat CSTRSimulation class
    - add comments along the code.
    - make ceq & teq global vars.
    - add step_time as config var for episode reset.
    - clarified the signal options for Cref_signal.
    - rename self.cnt into self.it_time, and added new self.max_trans_time.
    - add a reset of initial conditions for all env vars during reset.
    - remove declaration of steady state conditions at each step.
    - unify the declaration of signals 1 & 4.
    - modify vars with σ char to interpretable text instead.
    - rename episode_step to random_step.
    - remove signal generation in main.

--- pythonsim/bonsai-training-interface/sim/random_action_cstr_sim.py
+++ pythonsim/bonsai-training-interface/sim/random_action_cstr_sim.py
@@ -1,3 +1,4 @@
+import traceback
 import json
 import os
 import random
@@ -6,7 +7,7 @@ from scipy import interpolate
 import math
 
 
-import cstr_model as cstr
+from cstr_model import CSTR_Solver as CSTR_Solver
 
 import numpy as np
 import sys
@@ -14,13 +15,23 @@ import sys
 import matplotlib.pyplot as plt
 
 # time step (seconds) between state updates
-Δt = 1
+Δt_sim = 1
+thermal_runaway = 400
+
+# Known equilibrion relationships for concentration and temperature
+Cr_eq = [8.57, 6.9275, 5.2850, 3.6425, 2]
+Tr_eq = [311.2612, 327.9968, 341.1084, 354.7246, 373.1311]
+# TODO: Add the equilibrium values for Tc_eq
+Tc_eq = [292, 292, 292, 292, 292]
 
 class CSTRSimulation():
     def reset(
         self,
-        Cref_signal: float = 0,
+        Cref_signal: float = 3,
         noise_percentage: float = 0,
+        step_time: float = Δt_sim,
+
+        # Non-applicable initialization parameters during episode restart
         ΔTc: float = 0,
         Ca: float = 0,
         T: float = 0,
@@ -31,107 +42,135 @@ class CSTRSimulation():
     ):
         """
         CSTR model for simulation.
+
+        Cref_signal: Reference Cref & Tref to be followed by our control.
+          Values that Cref_signal can take:
+            (0) N/A  ---> STATIC: Cref == Tref == 0.
+            (1) Transition from (Cref, Tref) of (8.57, 311.3) to (2, 373.1), from its 0 to 52.
+            (2) STEADY STATE: (Cref, Tref) sustained at (2, 373.1).
+            (3) STEADY STATE: (Cref, Tref) sustained at (8.57, 311.3).
+            (4) Transition from (Cref, Tref) of (8.57, 311.3) to (2, 373.1), from its 22 to 74.
         """
-        #initial conditions
-        Ca0: float = 8.5698 #kmol/m3
-        T0: float = 311.2639 #K
-        Tc0: float = 292 #K
-        
-        self.T = T0
+        # Default initial conditions
+        Ca0: float = 8.5698     #kmol/m3
+        T0: float = 311.2639    #K
+        Tc0: float = 292        #K
+
+        # Initialize environment conditions -- overwritten later based on Cref_signal.
+        self.Cr = Ca0
+        self.Tr = T0
         self.Tc = Tc0
-        self.Ca = Ca0
-        self.ΔTc = 0
 
-        self.cnt = 0
+        # Initialize auxiliary variables.
+        self.ΔTc = 0                # Action parsed from the brain.
+        self.max_trans_time = 90    # Max considered transition time (the simulation can run further theoretically).
+        self.it_time = 0            # Initialize current simulation time.
 
+        # Store configuration received for current episode.
         self.Cref_signal = Cref_signal
         self.noise_percentage = noise_percentage
+        self.step_time = step_time
         
-        if self.Cref_signal == 2:
-            self.Cref = 2
-            self.Tref = 373.1311
-            self.Ca = 2
-            self.T = 373.1311
-        else:
-            self.Cref = 8.5698
-            self.Tref = 311.2612
+
+        ## INITIALIZE ENVIRONMENT VARIABLES BASED ON VALUE OF Cref_signal.
+
+        # TODO: Do we really need Cref_signal == 0?
+        # Declare initial conditions for current signal selection.
+        if self.Cref_signal == 0:
+            self.Cref = 0
+            self.Tref = 0
+            # TODO: What do we do with initialization values when Cref_signal == 0?
+            self.Cr = 8.5698
+            self.Tr = 311.2612
+            self.Tc = 292
+        
+        # Transition from (Cref, Tref) of (8.57, 311.3) to (2, 373.1).
+        # Declare initial conditions for current signal selection.
+        elif self.Cref_signal == 1 \
+             or self.Cref_signal == 4:
+            self.Cr = 8.5698
+            self.Tr = 311.2612
+            self.Tc = 292
+            self.Cref = self.Cr
+            self.Tref = self.Tr
+        
+        # STEADY STATE: (Cref, Tref) sustained at (2, 373.1).
+        # Declare initial conditions for current signal selection.
+        elif self.Cref_signal == 2:
+            self.Cr = 2
+            self.Tr = 373.1311
+            self.Tc = 0 # TODO: Update correct value of coolant temp.
+            self.Cref = self.Cr
+            self.Tref = self.Tr
+        
+        # STEADY STATE: (Cref, Tref) sustained at (8.57, 311.3).
+        # Declare initial conditions for current signal selection.
+        elif self.Cref_signal == 3:
+            self.Cr = 8.5698
+            self.Tr = 311.2612
+            self.Tc = 292
+            self.Cref = self.Cr
+            self.Tref = self.Tr
 
         
 
 
     def step(self, ΔTc: float):
-        if self.Cref_signal == 0:
-            self.Cref = 0
-            self.Tref = 0
-        elif self.Cref_signal == 1:
-            #update Cref an Tref
-            time = 90
+        # Transition from (Cref, Tref) of (8.57, 311.3) to (2, 373.1).
+        if self.Cref_signal == 1 \
+            or self.Cref_signal == 4:
+
+            # Select the transient data desired.
             p1 = 22 
             p2 = 74
-            k = self.cnt+p1
-            ceq = [8.57,6.9275,5.2850,3.6425,2]
-            teq = [311.2612,327.9968,341.1084,354.7246,373.1311]
-            C = interpolate.interp1d([0,p1,p2,time], [8.57,8.57,2,2])
-            self.Cref = float(C(k))
-            T_ = interpolate.interp1d([0,p1,p2,time], [311.2612,311.2612,373.1311,373.1311])
-            self.Tref = float(T_(k))
-        elif self.Cref_signal == 2: #steady state 1
-            self.Cref = 2
-            self.Tref = 373.1311
-        elif self.Cref_signal == 3: #steady state 2
-            self.Cref = 8.5698
-            self.Tref = 311.2612
-        elif self.Cref_signal == 4: #full sim
-            k = self.cnt
-            time = 90
-            #update Cref an Tref
-            p1 = 22 
-            p2 = 74 
-            ceq = [8.57,6.9275,5.2850,3.6425,2]
-            teq = [311.2612,327.9968,341.1084,354.7246,373.1311]
-            C = interpolate.interp1d([0,p1,p2,time], [8.57,8.57,2,2])
-            self.Cref = float(C(k))
-            T_ = interpolate.interp1d([0,p1,p2,time], [311.2612,311.2612,373.1311,373.1311])
-            self.Tref = float(T_(k))
+            C_sched = interpolate.interp1d([0,p1,p2,self.max_trans_time], [8.57,8.57,2,2])
+            T_sched = interpolate.interp1d([0,p1,p2,self.max_trans_time], [311.2612,311.2612,373.1311,373.1311])
             
+            # Store the current iteration in auxiliary variable.
+            k = self.it_time
+            if self.Cref_signal == 1:
+                # Update for Cref_signal==1, so transition starts right away.
+                k = self.it_time + p1
+
+            # Define the reference value for current iteration.
+            self.Cref = float(C_sched(k))
+            self.Tref = float(T_sched(k))
+        
+        # Update the latest stored  action received.
         self.ΔTc = ΔTc
         
+        # Define step to introduce to states based on noise_percentage.
+        C_max_range = (8.5698 - 2)
+        T_max_range = ( 373.1311 - 311.2612)
         error_var = self.noise_percentage
-        σ_max1 = error_var * (8.5698 - 2)
-        σ_max2 = error_var * ( 373.1311 - 311.2612)
+        Cr_error = error_var * random.uniform(-C_max_range, C_max_range)
+        Tr_error = error_var * random.uniform(-T_max_range, T_max_range)
 
-        σ_Ca = random.uniform(-σ_max1, σ_max1)
-        σ_T = random.uniform(-σ_max2, σ_max2)
-        mu = 0
+        # Call the CSTR solver.
+        model = CSTR_Solver(Tr = self.Tr, Cr = self.Cr, Tc = self.Tc, ΔTc = self.ΔTc)
 
-        #calling the CSTR python model
-        model = cstr.CSTRModel(T = self.T, Ca = self.Ca, Tc = self.Tc, ΔTc = self.ΔTc)
+        # EXTRACT UPDATED VALUES FROM SOLVER.    
+        #self.Tc += self.ΔTc
+        self.Tc = model.Tc              # Tc - Temperature of the coolant.
+        self.Tr = model.Tr + Tr_error   # Tr - Temperature of the reactor's output.
+        self.Cr = model.Cr + Cr_error   # Cr - Concentration of the reactor's output.
 
-        #Tc
-        self.Tc += self.ΔTc
+        # Increase the current iteration time by the stepping time.
+        self.it_time += self.step_time
 
-        #Tr
-        self.T = model.T + σ_T
-
-        #Ca
-        self.Ca = model.Ca + σ_Ca
-
-        #Increase time
-        self.cnt += 1
-
-    def episode_step(self) -> None:
+    def random_step(self) -> None:
         #self.step(action.get("Tc_adjust"))
-        #Random actions
         self.step(np.random.randint(-10,10))
         
 
     def get_state(self):
+        # TODO: Add variables WITHOUT noise to be able to compute real error for control analysis & brain reward.
         return {
-            "Tr": self.T,
-            "Tc": self.Tc,
-            "Cr": self.Ca,
-            "Tref": self.Tref,
-            "Cref": self.Cref,
+            "Cr": self.Cr,      # Concentration at the reactor's output (kmol/m3).
+            "Tr": self.Tr,      # Temperature at the reactor's output (Kelvin).
+            "Tc": self.Tc,      # Temperature of the coolant (Kelvin).
+            "Cref": self.Cref,  # Reference concentration desired by the operators at reactor's output (kmol/m3).
+            "Tref": self.Tref,  # Reference temperature desired by the operators at reactor's output (Kelvin).
         }
 
     def halted(self) -> bool:
@@ -141,11 +180,11 @@ class CSTRSimulation():
         elif self.Ca < 0:
             print("#### CONCENTRATION BELOW ZERO !! ###")
             return True
-        elif self.ΔTc > 10 or self.ΔTc < -10 :
+            elif self.ΔTc > 10 or self.ΔTc < -10 :
             print("#### PHYSICAL LIMITATION REACHED FOR DTc !! ###")
             return True
-        else:
-            return False
+            else:
+                return False
 
 def main():
 
@@ -161,33 +200,21 @@ def main():
     Ca_error = []
     Tref_error = []
     
+    # Run a full episode with random actions.
     time = 90
-    
     for k in range(time):
         if cstr_sim.halted():
             break
-
-        #update Cref and Tref
-        p1 = 22
-        p2 = 74 
-        ceq = [8.57,6.9275,5.2850,3.6425,2]
-        teq = [311.2612,327.9968,341.1084,354.7246,373.1311]
-        C = interpolate.interp1d([0,p1,p2,time], [8.57,8.57,2,2])
-        cstr_sim.Cref = float(C(k))
-        T_ = interpolate.interp1d([0,p1,p2,time], [311.2612,311.2612,373.1311,373.1311])
-        cstr_sim.Tref = float(T_(k))
-            
+        
         Cref_list.append(cstr_sim.Cref)
         Tref_list.append(cstr_sim.Tref)
         
-        #cstr_sim.episode_step(np.random.randint(-10,10))
-        #cstr_sim.episode_step(cstr_sim.ΔTc)
-        cstr_sim.episode_step()
+        cstr_sim.random_step()
         state = cstr_sim.get_state()
         
-        error = (cstr_sim.Ca - cstr_sim.Cref)**2
+        error = (cstr_sim.Cr - cstr_sim.Cref)**2
         Ca_error.append(error)
-        error = (cstr_sim.T - cstr_sim.Tref)**2
+        error = (cstr_sim.Tr - cstr_sim.Tref)**2
         Tref_error.append(error)
         
         T_list.append(state['Tr'])
