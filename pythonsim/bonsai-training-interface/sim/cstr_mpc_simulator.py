b2f6761bb3ea6bd88536cf10f4084b47799b511b
Juan Vergara <40925447+juanvergaramunoz@users.noreply.github.com>
2023-01-08 16:18:46 -0800

    Aggregate with non-linear-mpc
    - keep cstr_mpc_simulator.py as code base.
    - overlap non_linear_mpc_model.py, add the diffs, for later review.

--- pythonsim/bonsai-training-interface/sim/cstr_mpc_simulator.py
+++ pythonsim/bonsai-training-interface/sim/cstr_mpc_simulator.py
@@ -20,22 +20,8 @@ import do_mpc
 
 π = math.pi
 
-on_spec_list = []
-off_spec_list = []
-Ca_RMF_list = []
-Tr_RMF_list = []
-thermal_run  = []
-df_train = pd.DataFrame()
-#df_train = pd.read_csv('cstr_simulator_data.csv')
+def non_lin_mpc(noise, CrSP, Ca0, T0, Tc0):
 
-# Config
-anim = False
-noise = 0.05 #pct of noise
-sim_total = 1 # total simulations
-
-for idx in range(0,sim_total):
-    #print(idx)
-    df_op = pd.DataFrame()
     # CSTR Simulation Constants
     F = 1 #Volumetric flow rate (m3/h)
     V = 1 #Reactor volume (m3)
@@ -46,36 +32,36 @@ for idx in range(0,sim_total):
     phoCp = 500 #Density multiplied by heat capacity (kcal/(m3·K))
     UA = 150 #Overall heat transfer coefficient multiplied by tank area (kcal/(K·h))
     Cafin = 10 #kmol/m3
-    Tf = 298.2 #K    
-
-    #initial conditions
-    Ca0 = 8.5698 #kmol/m3
-    T0 = 311.2639 #K
-    Tc0 = 292 #K
-      
+    Tf = 298.2 #K
+    
+    # initial conditions <version CMS.py>
+    #Ca0 = 8.5698 #kmol/m3
+    #T0 = 311.2639 #K
+    #Tc0 = 292 #K
+        
     #MPC MODEL
-    model_type = 'continuous' # 'discrete' or 'continuous'
+    model_type = 'continuous' # either 'discrete' or 'continuous'
     model = do_mpc.model.Model(model_type)
     
-    # State - Controled variables CV
+    # State - Environment variables CV
     Ca = model.set_variable(var_type='_x', var_name='Ca', shape=(1,1)) #Concentration
     T = model.set_variable(var_type='_x', var_name='T', shape=(1,1)) #Temperature
 
-    # define measurements
+    # define measurements:
     model.set_meas('Ca', Ca, meas_noise=True)
     model.set_meas('T', T, meas_noise=True)
-    
+
     # Input - Manipulated Variable MV
     Tc = model.set_variable(var_type='_u', var_name='Tc') #cooling liquid temperature
 
-    # Time Varying parameters
+    # Non-fixed Time Varying parameters:
     Caf = model.set_variable(var_type='_tvp', var_name='Caf')
     Tref = model.set_variable(var_type='_tvp', var_name='Tref')
 
     # Process Dynamics Model
     model.set_rhs('Ca', (F/V * (Cafin - Ca)) - (k0 * exp(-E/(R*T))*Ca)  )
     model.set_rhs('T', (F/V *(Tf-T)) - ((ΔH/phoCp)*(k0 * exp(-E/(R*T))*Ca)) - ((UA /(phoCp*V)) *(T-Tc)) )
-    
+
     # Build the model
     model.setup()
 
@@ -85,39 +71,54 @@ for idx in range(0,sim_total):
         'n_horizon': 20,
         'n_robust': 1,
         'open_loop': 0,
+        #'t_step': 0.1, #<version NLMM.py>
         't_step': Δt,
         'store_full_solution': True,
+        # Use MA27 linear solver in ipopt for faster calculations:
+        #'nlpsol_opts': {'ipopt.linear_solver': 'MA27'} #<version NLMM.py>
     }
 
     mpc.set_param(**setup_mpc)
     # scaling variables
     mpc.scaling['_x', 'T'] = 100
     mpc.scaling['_u', 'Tc'] = 100
+    #mpc.scaling['_u', 'ω_s'] = 1
 
-    #SETUP OBJECTIVE - COST FUNCTION
+    # SETUP OBJECTIVE - COST FUNCTION
     _x = model.x
     _tvp = model.tvp
-    mterm = ((_x['Ca'] - _tvp['Caf']))**2 # terminal cost
-    lterm = ((_x['Ca'] - _tvp['Caf']))**2 # stage cost
+    _u = model.u #<version NLMM.py>
+    # <version cstr_mpc_simulator.py (CMS.py)>
+    #mterm = ((_x['Ca'] - _tvp['Caf']))**2 # terminal cost
+    #lterm = ((_x['Ca'] - _tvp['Caf']))**2 # stage cost
+    # <version non_linear_mpc_model.py (NLMM.py)>
+    mterm = ((_x['Ca'] - CrSP))**2 # terminal cost
+    lterm = ((_x['Ca'] - CrSP))**2 # stage cost
 
     mpc.set_objective(mterm=mterm, lterm=lterm)
 
-    mpc.set_rterm(Tc = 1.5 * 1) # input penalty
+    mpc.set_rterm(Tc = 1.5 * 1) # input penalty 1e-2 , 1000
 
     # SETUP CONSTRAINTS
     # constraints on states
     mpc.bounds['lower', '_x', 'Ca'] = 0.1
     mpc.bounds['upper', '_x', 'Ca'] = 12
-    mpc.bounds['upper', '_x', 'T'] = 400 
+    
+    mpc.bounds['upper', '_x', 'T'] = 400
     mpc.bounds['lower', '_x', 'T'] = 100
-    
+
+    #mpc.set_nl_cons('Ca', _x['Ca'], ub=2, soft_constraint=True, penalty_term_cons=1e4) #<version NLMM.py>
+
     # lower bounds of the inputs
+    #mpc.bounds['lower', '_u', 'dTc'] = -10 #<version NLMM.py>
     mpc.bounds['lower', '_u', 'Tc'] = 273 #273
-    
+
     # upper bounds of the inputs
+    #mpc.bounds['upper', '_u', 'dTc'] = 10 #<version NLMM.py>
     mpc.bounds['upper', '_u', 'Tc'] = 322
 
     # TIME VARYING VALUES
+    # in optimizer configuration:
     tvp_temp_1 = mpc.get_tvp_template()
     tvp_temp_1['_tvp', :] = np.array([8.5698])
 
@@ -126,7 +127,8 @@ for idx in range(0,sim_total):
 
     tvp_temp_3 = mpc.get_tvp_template()
     tvp_temp_3['_tvp', :] = np.array([2])
-    
+
+
     def tvp_fun(t_now):
         #setpoint variables
         p1 = 22 # time to start transition
@@ -136,10 +138,11 @@ for idx in range(0,sim_total):
         teq = [311.2612,327.9968,341.1084,354.7246,373.1311]
         C = interpolate.interp1d([0,p1,p2,time], [8.57,8.57,2,2])
         T_ = interpolate.interp1d([0,p1,p2,time], [311.2612,311.2612,373.1311,373.1311])
-        
+
         if t_now < p1:
             return tvp_temp_1
         elif t_now >= p1 and t_now < p2:
+            #y = -0.2527*t_now + 11.097 #<version NLMM.py>
             y = float(C(k))
             tvp_temp_3['_tvp', :] = np.array([y])
             return tvp_temp_3
@@ -156,12 +159,15 @@ for idx in range(0,sim_total):
     #SIMULATOR
     simulator = do_mpc.simulator.Simulator(model)
     params_simulator = {
+        #'integration_tool': 'cvodes', #<version NLMM.py>
+        #'abstol': 1e-10, #<version NLMM.py>
+        #'reltol': 1e-10, #<version NLMM.py>
         't_step': Δt
     }
 
     simulator.set_param(**params_simulator)
 
-    #SETUP TV parameters
+    # SETUP TV parameters
     p_num = simulator.get_p_template()
     tvp_num = simulator.get_tvp_template()
 
@@ -169,11 +175,13 @@ for idx in range(0,sim_total):
     def tvp_fun(t_now):
         return tvp_num
 
+    # uncertain parameters
     def p_fun(t_now):
         return p_num
-    
+
     simulator.set_tvp_fun(tvp_fun)
     simulator.set_p_fun(p_fun)
+
 
     simulator.setup()
 
@@ -184,6 +192,8 @@ for idx in range(0,sim_total):
 
     u0 = simulator.u0
     u0['Tc'] = Tc0
+    #u0['Tc'] = TcSP #<version NLMM.py>
+
 
     mpc.x0 = x0
     simulator.x0 = x0
@@ -193,23 +203,23 @@ for idx in range(0,sim_total):
     simulator.u0 = u0
     estimator.u0 = u0
 
+
     mpc.set_initial_guess()
-    
-    #Simulate teps
+
+    # Simulate N steps
     Cref_vals = []
     Ca_error = []
     Tref_vals = []
     Tref_error = []
     u0_old = 0
-    time = 90 #simulate 45 minutes with 0.5 minute step
+    #<version CMS.py>
+    #time = 90 #simulate 45 minutes with 0.5 minute step
+    #<version NLMM.py>
+    time = 1
     for k in range(time):
         if k > 1:
             u0_old = u0[0][0]
-            #generate dataset for training
-            #df_t = state_ops[0]
-            #df_t.append(u0[0])
-            df_t = pd.DataFrame(state_ops, columns = ['Ca0','T0'])
-            df_t['Tc0'] = u0[0]
+           
             
         u0 = mpc.make_step(x0) # get MPC next action
         #limit controller actions from -10 to 10 degrees Celsius
@@ -217,30 +227,28 @@ for idx in range(0,sim_total):
             if u0[0][0] - u0_old > 10:
                 u0 = np.array([[u0_old + 10]])
             elif u0[0][0] - u0_old < -10:
-                u0 = np.array([[u0_old -10]])
-            
-        # Adding Noise
+                u0 = np.array([[u0_old - 10]])
+        else: #<version NLMM.py>
+            if u0[0][0] - Tc0 >= 10:
+                u0 = np.array([[Tc0 + 10]])
+            elif u0[0][0] - Tc0 <= -10:
+                u0 = np.array([[Tc0 - 10]])
+        
+        #Add Noise
+        #For random samples from N(mu,sigma**2), use: /
+        #sigma * np.random.randn(...) + mu
+        #error_var = 0.1
         error_var = noise
         σ_max1 = error_var * (8.5698 -2)
         σ_max2 = error_var * ( 373.1311 - 311.2612)
         mu = 0
         #v0 = mu + σ_max * np.random.randn(0, 1)
         v0 = np.array([mu + σ_max1* np.random.randn(1, 1)[0],mu + σ_max2* np.random.randn(1, 1)[0]])
+        
         y_next = simulator.make_step(u0,v0=v0) # MPC
 
         #get all state values
         state_ops = y_next.reshape((1,2))
-        #append in a table
-        df_ = pd.DataFrame(state_ops, columns = ['Ca','T'])
-        df_['Tc'] = u0[0]
-        df_op = df_op.append(df_)
-        if k > 1:
-            #generate dataset
-            df_t['Ca'] = df_['Ca']
-            df_t['T'] = df_['T']
-            df_t['Tc'] = df_['Tc']
-            df_t['dTc'] = df_['Tc'] - df_t['Tc0']
-            df_train = df_train.append(df_t)
 
         #Benchmark
         p1 = 22 # time to start transition
@@ -263,7 +271,7 @@ for idx in range(0,sim_total):
             
         Cref_vals.append(Cref)
         error = (y_next[0][0] - Cref)**2
-        Ca_error.append(error) 
+        Ca_error.append(error)
 
         Tref_vals.append(Tref)
         error = (y_next[1][0] - Tref)**2
@@ -271,64 +279,11 @@ for idx in range(0,sim_total):
         
         x0 = estimator.make_step(y_next) # Simulation next step
 
-    df_op = df_op.reset_index()
-    df_op['dTc'] = df_op['Tc'].diff()
-    if df_op['T'].max() >= 400:
-        print("###########THERMAL RUNAWAY#####")
-        thermal_run.append(1)
+    return u0
 
-    Ca_RMS = sqrt(np.average(Ca_error))
-    Tref_RMS = sqrt(np.average(Tref_error))
-    print("Ca RMF: ", Ca_RMS)
-    print("Tr RMF: ", Tref_RMS)
-    Ca_RMF_list.append(Ca_RMS)
-    Tr_RMF_list.append(Tref_RMS)
- 
-    if anim == True:
-        #Animation
-        mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
+
+
+#print(non_lin_mpc(noise = 0, CrSP = 8.57 , Ca0 = 8.5698 , T0 = 311.2639 , Tc0 = 292))
+
         
-        rcParams['axes.grid'] = True
-        rcParams['font.size'] = 18
-        fig, ax = plt.subplots(3, sharex=True, figsize=(16,12))
-        # Configure plot:
-        mpc_graphics.add_line(var_type='_x', var_name='Ca', axis=ax[0])
-        mpc_graphics.add_line(var_type='_x', var_name='T', axis=ax[1])
-        mpc_graphics.add_line(var_type='_u', var_name='Tc', axis=ax[2])
-        mpc_graphics.add_line(var_type='_tvp', var_name='Caf', axis=ax[0])
-        ax[0].set_ylabel('Ca and Caf')
-        ax[1].set_ylabel('T')
-        ax[2].set_ylabel('Tc')
-
-        # Update properties for all prediction lines:
-        for line_i in mpc_graphics.pred_lines.full:
-            line_i.set_linewidth(2)
-        # Highlight nominal case:
-        for line_i in np.sum(mpc_graphics.pred_lines['_x', :, :,0]):
-            line_i.set_linewidth(5)
-        for line_i in np.sum(mpc_graphics.pred_lines['_u', :, :,0]):
-            line_i.set_linewidth(5)
-
-        def update(t_ind):
-            print('Writing frame: {}.'.format(t_ind), end='\r')
-            mpc_graphics.plot_results(t_ind=t_ind)
-            mpc_graphics.plot_predictions(t_ind=t_ind)
-            mpc_graphics.reset_axes()
-            lines = mpc_graphics.result_lines.full
-            return lines
-
-        n_steps = mpc.data['_time'].shape[0]
-
-        anim = FuncAnimation(fig, update, frames=n_steps, blit=True)
-
-        gif_writer = ImageMagickWriter(fps=5)
-        anim.save('anim_CSTR.gif', writer=gif_writer)
-
-
-print("CaRMF mean: ", np.mean(Ca_RMF_list), "+- ", np.std(Ca_RMF_list))
-print("TRMF mean: ", np.mean(Tr_RMF_list), "+- ", np.std(Tr_RMF_list))
-print("Total thermal runaway: ", (np.sum(thermal_run)/sim_total)*100, "%")
-
-df_train.to_csv('cstr_simulator_data.csv', index=False)
-
 
