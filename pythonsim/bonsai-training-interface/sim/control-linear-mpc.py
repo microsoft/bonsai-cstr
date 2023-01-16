cc5669953796773900e3c10a6c630ad1dbb24c02
Juan Vergara <40925447+juanvergaramunoz@users.noreply.github.com>
2023-01-15 19:05:26 -0800

    Change linear_mpc to be subclass of CSTRSimulation - add tr_init as control action when operating CSTR sim with linear_mpc. - create reset function: (1) calls super's reset, (2) inits linear_mpc. - create step function: (1) run linear_mpc, (2) calls super's step. - create get_state method to add linear_mpc action to super's states. - modify per-iteration states to read directly from 'self'.

--- pythonsim/bonsai-training-interface/sim/control-linear-mpc.py
+++ pythonsim/bonsai-training-interface/sim/control-linear-mpc.py
@@ -4,14 +4,64 @@ from scipy.integrate import odeint
 from gekko import GEKKO
 from scipy import interpolate
 import math
+from typing import Dict, Any, Union
 import random
 
-from cstr_sim import CSTRSimulation
+from sim.cstr_sim import CSTRSimulation
 
 
-class linear_mpc:
+class linear_mpc(CSTRSimulation):
 
-    def __init__(self, state):
+    # def __init__():
+
+    def reset(
+        self,
+        config: Dict[str, Any] = {},
+    ):
+
+        super().reset(config)
+
+        # Initialize the control parameters based on new scenario.
+        self.init_control()
+    
+        # Initialize mpc actions.
+        self.tr_init = 2
+
+
+    def step(
+        self,
+        action: Dict[str, Any] = {}
+    ):
+
+        # Take in and apply MPC characteristics to be updated.
+        self.tr_init = 2
+        if "tr_init" in action.keys():
+            self.tr_init = action["tr_init"]
+        else:
+            print(f"No valid actions parsed to compute_action function in linear-mpc control. Action Dict == {action}")
+
+        # Run Linear MPC model.
+        new_Tc = self.compute_best_action(self.tr_init)
+        # Get action for simulation.
+        Tc_adjust = new_Tc - self.Tc
+        action = dict([("Tc_adjust", Tc_adjust)])
+
+        # Run simulation model with recommended action by Linear MPC.
+        super().step(action)
+        
+
+    def get_state(self):
+
+        # Get sim states.
+        states = super().get_state()
+
+        # Last tr_init used by MPC.
+        states["lin_mpc_tr_init"] = self.tr_init
+
+        return states
+
+
+    def init_control(self):
 
         self.remote_server = True
         self.display_mpc_vals = False
@@ -22,9 +72,9 @@ class linear_mpc:
         self.m.time = np.linspace(0, 45, num=90) #simulate 45 minutes (0.5 minute per controller action)
 
         # initial conditions
-        self.Tc0 = state["Tc"]
-        self.T0 = state["Tr_no_noise"]
-        self.C0 = state["Cr_no_noise"]
+        self.Tc0 = self.Tc
+        self.T0 = self.Tr_no_noise
+        self.C0 = self.Cr_no_noise
 
         # Manipulated and Controlled Variables
         self.m.Tc = self.m.MV(value = self.Tc0, lb=273, ub=322) #mv with constraints
@@ -56,18 +106,18 @@ class linear_mpc:
         return
     
 
-    def compute_action(self, state):
+    def compute_best_action(self, tr_init):
 
         # COMPUTATION OF MPC
         # insert measurement
-        self.m.T.MEAS = state["Tr_no_noise"]
+        self.m.T.MEAS = self.Tr_no_noise
         # update setpoint
-        self.m.T.SP = state["Tref"]
+        self.m.T.SP = self.Tref
 
         # solve MPC
         self.m.solve(disp=self.display_mpc_vals)
         # change to a fixed starting point for trajectory
-        self.m.T.TR_INIT = 2
+        self.m.T.TR_INIT = tr_init
 
         # retrieve new Tc values
         new_Tc = self.m.Tc.NEWVAL
