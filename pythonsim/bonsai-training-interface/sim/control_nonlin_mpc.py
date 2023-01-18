2884794338377545f5d6ab9bd658154f4d8a29aa
Juan Vergara <40925447+juanvergaramunoz@users.noreply.github.com>
2023-01-17 19:50:26 -0800

    update nonlin_mpc control - add reference to cstr_sim class. - make non_lin_mpc a subclass of CSTRSimulation. - add reset, step, and get_states methods as in linear_mpc class. -- (use XX placeholders for future tuning mpc variables.) - add reference to self.step_time. - make necessary variables become attributes to the subclass: -- self.model -- self.mpc -- self.estimator -- self.simulator

--- pythonsim/bonsai-training-interface/sim/control_nonlin_mpc.py
+++ pythonsim/bonsai-training-interface/sim/control_nonlin_mpc.py
@@ -11,6 +11,9 @@ import sys
 from casadi import *
 from scipy import interpolate
 
+from typing import Dict, Any, Union
+
+from sim.cstr_sim import CSTRSimulation
 from sim.cstr_solver import CSTR_Solver
 
 # Import do_mpc package:
@@ -18,230 +21,276 @@ import do_mpc
 
 
 # time step (seconds) between state updates
-Δt = 1
+#Δt = 1
 
 π = math.pi
 
-def non_lin_mpc(noise, CrSP, Cr0, T0, Tc0):
+class non_lin_mpc(CSTRSimulation):
+
+    # def __init__():
+
+    def reset(
+        self,
+        config: Dict[str, Any] = {},
+    ):
+
+        super().reset(config)
+
+        # Initialize the control parameters based on new scenario.
+        self.init_control()
+    
+        # Initialize mpc actions.
+        self.XX = 2
 
 
-    # Call the CSTR solver for mpc modeling.
-    solver = CSTR_Solver(Tr = T0,
-                         Cr = Cr0,
-                         Tc = Tc0,
-                         ΔTc = 0,
-                         step_time = Δt,
-                         edo_solver_n_its = 1,
-                         debug=False)
+    def step(
+        self,
+        action: Dict[str, Any] = {}
+    ):
+
+        # Take in and apply MPC characteristics to be updated.
+        self.XX = 2
+        if "XX" in action.keys():
+            self.XX = action["XX"]
+        else:
+            print(f"No valid actions parsed to compute_action function in nonlin-mpc control. Action Dict == {action}")
+
+        # Run Linear MPC model.
+        new_Tc = self.compute_best_action(self.XX)
+        # Get action for simulation.
+        Tc_adjust = new_Tc - self.Tc
+        action = dict([("Tc_adjust", Tc_adjust)])
+
+        # Run simulation model with recommended action by Linear MPC.
+        super().step(action)
         
-    #MPC MODEL
-    model_type = 'continuous' # either 'discrete' or 'continuous'
-    model = do_mpc.model.Model(model_type)
-    
-    # State - Environment variables CV
-    Cr = model.set_variable(var_type='_x', var_name='Cr', shape=(1,1)) # Concentration
-    Tr = model.set_variable(var_type='_x', var_name='Tr', shape=(1,1)) # Temperature
 
-    # define measurements:
-    model.set_meas('Cr', Cr, meas_noise=True)
-    model.set_meas('Tr', Tr, meas_noise=True)
+    def get_state(self):
 
-    # Input - Manipulated Variable MV
-    Tc = model.set_variable(var_type='_u', var_name='Tc') # Coolant temperature
+        # Get sim states.
+        states = super().get_state()
 
-    # Non-fixed Time Varying parameters:
-    Cref = model.set_variable(var_type='_tvp', var_name='Cref')
-    Tref = model.set_variable(var_type='_tvp', var_name='Tref')
+        # Last XX used by MPC.
+        states["nonlin_mpc_XX"] = self.XX
 
-    # Process Dynamics Model
-    model.set_rhs('Cr', solver.model_Cr(Cr, Tr) )
-    model.set_rhs('Tr', solver.model_Tr(Cr, Tr, Tc) )
+        return states
 
-    # Build the model
-    model.setup()
 
-    # SETUP CONTROLLER
-    mpc = do_mpc.controller.MPC(model)
-    setup_mpc = {
-        'n_horizon': 20,
-        'n_robust': 1,
-        'open_loop': 0,
-        #'t_step': 0.1, #<version NLMM.py>
-        't_step': Δt,
-        'store_full_solution': True,
-        # Use MA27 linear solver in ipopt for faster calculations:
-        #'nlpsol_opts': {'ipopt.linear_solver': 'MA27'} #<version NLMM.py>
-    }
 
-    mpc.set_param(**setup_mpc)
-    # scaling variables
-    mpc.scaling['_x', 'Tr'] = 100
-    mpc.scaling['_u', 'Tc'] = 100
-    #mpc.scaling['_u', 'ω_s'] = 1
+    def init_control(self):
 
-    # SETUP OBJECTIVE - COST FUNCTION
-    _x = model.x
-    _tvp = model.tvp
-    _u = model.u #<version NLMM.py>
-    # <version cstr_mpc_simulator.py (CMS.py)>
-    #mterm = ((_x['Cr'] - _tvp['Cref']))**2 # terminal cost
-    #lterm = ((_x['Cr'] - _tvp['Cref']))**2 # stage cost
-    # <version non_linear_mpc_model.py (NLMM.py)>
-    mterm = ((_x['Cr'] - CrSP))**2 # terminal cost
-    lterm = ((_x['Cr'] - CrSP))**2 # stage cost
+        Cr0 = self.Cr
+        Tr0 = self.Tr
+        Tc0 = self.Tc
 
-    mpc.set_objective(mterm=mterm, lterm=lterm)
+        # Call the CSTR solver for mpc modeling.
+        solver = CSTR_Solver(Tr = Tr0,
+                             Cr = Cr0,
+                             Tc = Tc0,
+                             ΔTc = 0,
+                             step_time = self.step_time,
+                             edo_solver_n_its = 1,
+                             debug=False)
+            
+        # MPC MODEL
+        self.model_type = 'continuous' # either 'discrete' or 'continuous'
+        self.model = do_mpc.model.Model(self.model_type)
+        
+        # State - Environment variables CV
+        Cr = self.model.set_variable(var_type='_x', var_name='Cr', shape=(1,1)) # Concentration
+        Tr = self.model.set_variable(var_type='_x', var_name='Tr', shape=(1,1)) # Temperature
 
-    mpc.set_rterm(Tc = 1.5 * 1) # input penalty 1e-2 , 1000
+        # define measurements:
+        self.model.set_meas('Cr', Cr, meas_noise=True)
+        self.model.set_meas('Tr', Tr, meas_noise=True)
 
-    # SETUP CONSTRAINTS
-    # constraints on states
-    mpc.bounds['lower', '_x', 'Cr'] = 0.1
-    mpc.bounds['upper', '_x', 'Cr'] = 12
-    
-    mpc.bounds['upper', '_x', 'Tr'] = 400
-    mpc.bounds['lower', '_x', 'Tr'] = 100
+        # Input - Manipulated Variable MV
+        Tc = self.model.set_variable(var_type='_u', var_name='Tc') # Coolant temperature
 
-    #mpc.set_nl_cons('Cr', _x['Cr'], ub=2, soft_constraint=True, penalty_term_cons=1e4) #<version NLMM.py>
+        # Non-fixed Time Varying parameters:
+        Cref = self.model.set_variable(var_type='_tvp', var_name='Cref')
+        Tref = self.model.set_variable(var_type='_tvp', var_name='Tref')
 
-    # lower bounds of the inputs
-    #mpc.bounds['lower', '_u', 'dTc'] = -10 #<version NLMM.py>
-    mpc.bounds['lower', '_u', 'Tc'] = 273 #273
+        # Process Dynamics Model
+        self.model.set_rhs('Cr', solver.model_Cr(Cr, Tr) )
+        self.model.set_rhs('Tr', solver.model_Tr(Cr, Tr, Tc) )
 
-    # upper bounds of the inputs
-    #mpc.bounds['upper', '_u', 'dTc'] = 10 #<version NLMM.py>
-    mpc.bounds['upper', '_u', 'Tc'] = 322
+        # Build the model
+        self.model.setup()
+
+        # SETUP CONTROLLER
+        self.mpc = do_mpc.controller.MPC(self.model)
+        setup_mpc = {
+            'n_horizon': 20,
+            'n_robust': 1,
+            'open_loop': 0,
+            #'t_step': 0.1, #<version NLMM.py>
+            't_step': self.step_time,
+            'store_full_solution': True,
+            # Use MA27 linear solver in ipopt for faster calculations:
+            #'nlpsol_opts': {'ipopt.linear_solver': 'MA27'} #<version NLMM.py>
+        }
+
+        self.mpc.set_param(**setup_mpc)
+        # scaling variables
+        self.mpc.scaling['_x', 'Tr'] = 100
+        self.mpc.scaling['_u', 'Tc'] = 100
+        #mpc.scaling['_u', 'ω_s'] = 1
+
+        # SETUP OBJECTIVE - COST FUNCTION
+        _x = self.model.x
+        _tvp = self.model.tvp
+        _u = self.model.u #<version NLMM.py>
+        # <version cstr_mpc_simulator.py (CMS.py)>
+        mterm = ((_x['Cr'] - _tvp['Cref']))**2 # terminal cost
+        lterm = ((_x['Cr'] - _tvp['Cref']))**2 # stage cost
+        # <version non_linear_mpc_model.py (NLMM.py)>
+        #mterm = ((_x['Cr'] - CrSP))**2 # terminal cost
+        #lterm = ((_x['Cr'] - CrSP))**2 # stage cost
+
+        self.mpc.set_objective(mterm=mterm, lterm=lterm)
+
+        self.mpc.set_rterm(Tc = 1.5 * 1) # input penalty 1e-2 , 1000
+
+        # SETUP CONSTRAINTS
+        # constraints on states
+        self.mpc.bounds['lower', '_x', 'Cr'] = 0.1
+        self.mpc.bounds['upper', '_x', 'Cr'] = 12
+        
+        self.mpc.bounds['upper', '_x', 'Tr'] = 400
+        self.mpc.bounds['lower', '_x', 'Tr'] = 100
+
+        #mpc.set_nl_cons('Cr', _x['Cr'], ub=2, soft_constraint=True, penalty_term_cons=1e4) #<version NLMM.py>
+
+        # lower bounds of the inputs
+        #mpc.bounds['lower', '_u', 'dTc'] = -10 #<version NLMM.py>
+        self.mpc.bounds['lower', '_u', 'Tc'] = 273 #273
+
+        # upper bounds of the inputs
+        #mpc.bounds['upper', '_u', 'dTc'] = 10 #<version NLMM.py>
+        self.mpc.bounds['upper', '_u', 'Tc'] = 322
 
         # TIME VARYING PARAMETERS
-    # in optimizer configuration:
-        tvp_temp = mpc.get_tvp_template()
+        # in optimizer configuration:
+        tvp_temp = self.mpc.get_tvp_template()
         tvp_temp['_tvp', :] = np.array([2])
 
-    def tvp_fun(t_now):
+        def tvp_fun(t_now):
             aux_Cref, aux_Tref = self.update_references(t_now)
             y = float(aux_Cref)
             tvp_temp['_tvp', :] = np.array([y])
             return tvp_temp
 
-        mpc.set_tvp_fun(tvp_fun)
+        self.mpc.set_tvp_fun(tvp_fun)
 
-    mpc.setup()
+        # Initialize MPC setup
+        self.mpc.setup()
 
-    #ESTIMATOR
-    estimator = do_mpc.estimator.StateFeedback(model)
+        # ESTIMATOR
+        self.estimator = do_mpc.estimator.StateFeedback(self.model)
 
-    #SIMULATOR
-    simulator = do_mpc.simulator.Simulator(model)
-    params_simulator = {
-        #'integration_tool': 'cvodes', #<version NLMM.py>
-        #'abstol': 1e-10, #<version NLMM.py>
-        #'reltol': 1e-10, #<version NLMM.py>
-        't_step': Δt
-    }
+        # SIMULATOR
+        self.simulator = do_mpc.simulator.Simulator(self.model)
+        params_simulator = {
+            #'integration_tool': 'cvodes', #<version NLMM.py>
+            #'abstol': 1e-10, #<version NLMM.py>
+            #'reltol': 1e-10, #<version NLMM.py>
+            't_step': self.step_time
+        }
 
-    simulator.set_param(**params_simulator)
+        self.simulator.set_param(**params_simulator)
 
-    # SETUP TV parameters
-    p_num = simulator.get_p_template()
-    tvp_num = simulator.get_tvp_template()
+        # SETUP TV parameters
+        p_num = self.simulator.get_p_template()
+        tvp_num = self.simulator.get_tvp_template()
 
-    # function for time-varying parameters
-    def tvp_fun(t_now):
-        return tvp_num
+        # function for time-varying parameters
+        def tvp_fun(t_now):
+            return tvp_num
 
-    # uncertain parameters
-    def p_fun(t_now):
-        return p_num
+        # uncertain parameters
+        def p_fun(t_now):
+            return p_num
 
-    simulator.set_tvp_fun(tvp_fun)
-    simulator.set_p_fun(p_fun)
+        self.simulator.set_tvp_fun(tvp_fun)
+        self.simulator.set_p_fun(p_fun)
+
+        # Initialize simulator setup
+        self.simulator.setup()
+
+        # INITIALIZE ENVIRONMENT STATES
+        # Set the initial state (Cr, Tr) of mpc, simulator and estimator:
+        x0 = self.simulator.x0
+        x0['Cr'] = Cr0
+        x0['Tr'] = Tr0
+
+        self.mpc.x0 = x0
+        self.simulator.x0 = x0
+        self.estimator.x0 = x0
+
+        # Set the initial action (Tc) of mpc, simulator and estimator:
+        u0 = self.simulator.u0
+        u0['Tc'] = Tc0
+
+        self.mpc.u0 = u0
+        self.simulator.u0 = u0
+        self.estimator.u0 = u0
+
+        # Initialize MPC Control
+        self.mpc.set_initial_guess()
 
 
-    simulator.setup()
 
-    # Set the initial state of mpc, simulator and estimator:
-    x0 = simulator.x0
-    x0['Cr'] = Cr0
-    x0['Tr'] = T0
+    def compute_best_action(self):
 
-    u0 = simulator.u0
-    u0['Tc'] = Tc0
-    #u0['Tc'] = TcSP #<version NLMM.py>
-
-
-    mpc.x0 = x0
-    simulator.x0 = x0
-    estimator.x0 = x0
-
-    mpc.u0 = u0
-    simulator.u0 = u0
-    estimator.u0 = u0
-
-
-    mpc.set_initial_guess()
-
-    # Simulate N steps
-    Cref_vals = []
-    Cr_error = []
-    Tref_vals = []
-    Tref_error = []
-    u0_old = 0
-    #<version CMS.py>
-    #time = 90 #simulate 45 minutes with 0.5 minute step
-    #<version NLMM.py>
-    time = 1
-    for k in range(time):
+        k = int(self.it_time/self.step_time)
+        
+        u0_old = 0
         if k > 1:
-            u0_old = u0[0][0]
-           
-            
-        u0 = mpc.make_step(x0) # get MPC next action
+            u0_old = self.u0[0][0]
+        
+        
+        # INITIALIZE ENVIRONMENT STATES
+        # Set the initial state (Cr, Tr) of mpc, simulator and estimator:
+        x0 = self.simulator.x0
+        x0['Cr'] = self.Cr
+        x0['Tr'] = self.Tr
+        self.u0 = self.mpc.make_step(x0) # get MPC next action
         #limit controller actions from -10 to 10 degrees Celsius
         if k > 1:
-            if u0[0][0] - u0_old > 10:
-                u0 = np.array([[u0_old + 10]])
-            elif u0[0][0] - u0_old < -10:
-                u0 = np.array([[u0_old - 10]])
+            if self.u0[0][0] - u0_old > 10:
+                self.u0 = np.array([[u0_old + 10]])
+            elif self.u0[0][0] - u0_old < -10:
+                self.u0 = np.array([[u0_old - 10]])
         else: #<version NLMM.py>
-            if u0[0][0] - Tc0 >= 10:
-                u0 = np.array([[Tc0 + 10]])
-            elif u0[0][0] - Tc0 <= -10:
-                u0 = np.array([[Tc0 - 10]])
+            if self.u0[0][0] - self.Tc >= 10:
+                self.u0 = np.array([[self.Tc + 10]])
+            elif self.u0[0][0] - self.Tc <= -10:
+                self.u0 = np.array([[self.Tc - 10]])
         
         #Add Noise
         #For random samples from N(mu,sigma**2), use: /
         #sigma * np.random.randn(...) + mu
         #error_var = 0.1
-        error_var = noise
+        error_var = self.noise_percentage
         σ_max1 = error_var * (8.5698 -2)
         σ_max2 = error_var * ( 373.1311 - 311.2612)
         mu = 0
         #v0 = mu + σ_max * np.random.randn(0, 1)
         v0 = np.array([mu + σ_max1* np.random.randn(1, 1)[0],mu + σ_max2* np.random.randn(1, 1)[0]])
         
-        y_next = simulator.make_step(u0,v0=v0) # MPC
+        y_next = self.simulator.make_step(self.u0,v0=v0) # MPC
 
-        #get all state values
+        # Get all state values.
         state_ops = y_next.reshape((1,2))
-
-            # Benchmark
-            Cref, Tref = self.update_references()
-            
-        Cref_vals.append(Cref)
-        error = (y_next[0][0] - Cref)**2
-        Cr_error.append(error)
-
-        Tref_vals.append(Tref)
-        error = (y_next[1][0] - Tref)**2
-        Tref_error.append(error)
         
-        x0 = estimator.make_step(y_next) # Simulation next step
+        self.x0 = self.estimator.make_step(y_next) # Simulation next step
 
-    return u0
+        return self.u0
 
 
 
-#print(non_lin_mpc(noise = 0, CrSP = 8.57 , Cr0 = 8.5698 , T0 = 311.2639 , Tc0 = 292))
+#print(non_lin_mpc(noise = 0, CrSP = 8.57 , Cr0 = 8.5698 , Tr0 = 311.2639 , Tc0 = 292))
 
         
 
